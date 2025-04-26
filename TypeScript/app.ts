b556516c51be497846ff36773a49f19cef671064
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-04-26 23:59:22 +0530

    Some more small core TypeScript Learnings

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -46,7 +46,7 @@ getDataOfUser({ id: 1, name: "John", age: 32, admin: true });
 // Type Aliases //
 type value = string | number;                 // Type Alias
 
-function abc(a: value) {                       // Function with type alias as parameter
+function abc(a: value) {                      // Function with type alias as parameter
     return a;
 }
 
@@ -65,18 +65,148 @@ function getDetails(user: newAdmin) {       // Function with type alias as param
 }
 
 
-// Classes //
+// Classes & Constructors //
 class Device {                              // Class Declaration with constructor
     name: string;
     price: number;
     color: string;
-    constructor(name:string, price: number, color:string) {
-        this.name = name;                   
+    constructor(name: string, price: number, color: string) {
+        this.name = name;
         this.price = price;
         this.color = color;
     }
 }
 
-let d1 = new Device("LG", 2000, "White");   
-let d2 = new Device("Samsung", 3000, "Black");                      
+let d1 = new Device("LG", 2000, "White");
+let d2 = new Device("Samsung", 3000, "Black");
 
+
+// Access Modifiers & Readonly // 
+class BottleMaker {
+    private halua: string = " Halua";        // Private Property
+    protected mithai: string = " Mithai";    // Protected Property
+    constructor(public name: string) { }   // Public Property
+}
+class MetalBottleMaker extends BottleMaker {
+    constructor(name: string) {
+        super(name);
+    }
+    getValue() {
+        console.log(this.name, this.mithai);
+    }
+}
+
+let b1 = new MetalBottleMaker("Bottle Maker");
+b1.getValue();
+
+
+// Readonly, Optional & Parameter Properties //
+class UserDetails {
+    constructor(public readonly name: string, public age: number, public email?: string) { // Optional Parameter
+        this.name = name;
+    }
+}
+
+
+// Getters & Setters //
+class Person {
+    constructor(public _name: string, public age: number) { }
+
+    get name() {                        // Getter
+        return this._name;
+    }
+    set name(value: string) {           // Setter
+        this._name = value;
+    }
+}
+
+let p1 = new Person("John", 25);
+
+
+// Static Members //
+class SheryJS {
+    static version: number = 1.15;      // Static Property
+    static getMath() {                  // Static Method
+        return Math.random();
+    }
+}
+
+
+// Abstract Classes & Methods //
+abstract class Payment {
+    constructor(protected amount: number) { }           // Abstract Class
+    abstract getPaymentValid(amount: number): void;     // Abstract Method
+}
+
+class Paytm extends Payment {
+    constructor(amount: number) {
+        super(amount);
+    }
+    getPaymentValid(amount: number) {                   // Implementing Abstract Method
+        console.log("Paytm Payment Validated", amount);
+    }
+}
+
+
+// Functions in TypeScript //
+function callB(name: string, cb: (value: string) => void) {
+    console.log(name);
+    cb(name);                                    // Callback function
+}
+callB("John", (name: string) => {
+    console.log(name);
+});
+
+
+// Optional & Default Parameters //
+function abcdef(name: string, gender?: string, age: number = 25) {
+    console.log(name, gender, age);
+}
+abcdef("John", "Male");                          // Optional Parameter
+
+
+// Rest Parameters //
+function sum(...arr: number[]) {                 // Rest Parameter
+    let total = arr.reduce((a,b) => a + b, 0);
+    console.log(total);
+}
+sum(1, 2, 3, 4, 5);                    
+
+
+// Function Overloading //
+function abd(a: string): void;
+function abd(a: string, b: number): number;
+function abd(a: any, b?: any): any {
+    if(typeof a === "string" && b === undefined) {
+        console.log(a);
+    }
+    else if(typeof a === "string" && typeof b === "number") {
+        return a.length + b;
+    }
+}
+abd("Hello");                          // Function Overloading
+abd("Hello", 5);                       // Function Overloading
+
+
+// Generics //
+function log<T>(val: T) {              // Generic Function
+    console.log(val);
+}
+log("Hey");
+
+interface Halwa<T> {                   // Generic Interface
+    name: string;
+    price: number;
+    key: T
+}
+function getHalwa(obj: Halwa<string>) {};
+getHalwa({ name: "Lauki Kalwa", price: 200, key: "Lauki" });
+
+class BatliMaker<T> {                  // Generic Classes
+    constructor(public key: T) {}
+}
+let bt1 = new BatliMaker("NewBatli");
+let bt2 = new BatliMaker(1000);
+
+
+// Modules //
